2a8860547c04c62d7d83ca068ccddf3502313be1
WRall <wm_rall@hotmail.com>
2015-01-12 20:38:09 -0800

    Fix max velocity from encoders

--- irs2015/src/org/usfirst/frc/team1318/robot/TuningConstants.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/TuningConstants.java
@@ -33,8 +33,8 @@ public class TuningConstants
     public static final double DRIVETRAIN_DEAD_ZONE = 0.1;
     public static final double DRIVETRAIN_MAX_POWER_LEVEL = 0.8; // max power level
 
-    public static final double DRIVETRAIN_LEFT_ENCODER_MAX_SPEED = 1.0; // max speed we expect to detect from the left encoder
-    public static final double DRIVETRAIN_RIGHT_ENCODER_MAX_SPEED = 1.0; // max speed we expect to detect from the right encoder
+    public static final double DRIVETRAIN_LEFT_ENCODER_MAX_SPEED = 170.0; // max speed we expect to detect from the left encoder
+    public static final double DRIVETRAIN_RIGHT_ENCODER_MAX_SPEED = 170.0; // max speed we expect to detect from the right encoder
 
     public static final double DRIVETRAIN_A = 0.2; // "a" coefficient (advancing turn)
     public static final double DRIVETRAIN_B = 1.0; // "b" coefficient (in-place turn)
