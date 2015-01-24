c4e35b256759b68a9f7d7a247837e0f0c6bbf578
grclaeys <grclaeys@gmail.com>
2015-01-24 11:45:22 -0800

    idk

--- irs2015/src/org/usfirst/frc/team1318/robot/DriveTrain/Intake/IntakeComponent.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/DriveTrain/Intake/IntakeComponent.java
@@ -1,6 +1,16 @@
 package org.usfirst.frc.team1318.robot.DriveTrain.Intake;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Talon;
+
 public class IntakeComponent
 {
 
+    private Talon motor;
+    private Encoder encoder;
+
+    //    public IntakeComponent() 
+    //    }
+    //       
+    //    }
 }
