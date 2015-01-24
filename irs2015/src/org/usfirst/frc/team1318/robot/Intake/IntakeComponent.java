2cc9473ac3082d426ff05d1fe15c87cb91ff1401
grclaeys <grclaeys@gmail.com>
2015-01-24 11:59:23 -0800

    fixed name

---
+++ irs2015/src/org/usfirst/frc/team1318/robot/Intake/IntakeComponent.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team1318.robot.Intake;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Talon;
+
+public class IntakeComponent
+{
+
+    private Talon motor;
+    private Encoder encoder;
+
+    //    public IntakeComponent(Talon motor, Talon encoder)
+    //    {
+    //        this.motor = motor(ElectronicsConstants.SOLENOID_MODULE_PORT_1);
+    //        this.encoder = encoder(ElectronicsConstants.DRIVETRAIN_LEFT_ENCODER_CHANNEL_A);
+    //    }
+}
