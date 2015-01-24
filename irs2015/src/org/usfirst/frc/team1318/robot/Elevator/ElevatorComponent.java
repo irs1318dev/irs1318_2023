ee2e0d13e787a1d73e7a9f688395a9b142ff4dbf
Preston <pfhgetty1337@gmail.com>
2015-01-24 11:16:10 -0800

    Did stuff -Elevator

---
+++ irs2015/src/org/usfirst/frc/team1318/robot/Elevator/ElevatorComponent.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team1318.robot.Elevator;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Talon;
+
+public class ElevatorComponent
+{
+    private Talon motor;
+    private Encoder encoder;
+
+    public ElevatorComponent()
+    {
+
+    }
+}
