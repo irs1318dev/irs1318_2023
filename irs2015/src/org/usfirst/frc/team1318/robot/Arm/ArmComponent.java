b63b91ea4af176992a9de61ac306df231e2554af
Tali <Tali@Takas>
2015-01-26 17:13:43 -0800

    Component with comments

--- irs2015/src/org/usfirst/frc/team1318/robot/Arm/ArmComponent.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/Arm/ArmComponent.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1318.robot.Arm;
 
 import org.usfirst.frc.team1318.robot.ElectronicsConstants;
+import org.usfirst.frc.team1318.robot.Common.SmartDashboardLogger;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -11,6 +12,12 @@ public class ArmComponent
     public DoubleSolenoid moveLinkage;
     public DoubleSolenoid extendLinkage;
 
+    //logging constants
+    private static final String TROMBONE_STATE_LOG_KEY = "dt.TromboneState";
+    private static final String MOVE_LINKAGE_STATE_LOG_KEY = "dt.MoveLinkageState";
+    private static final String EXTEND_LINKAGE_STATE_LOG_KEY = "dt.ExtendLinkageState";
+
+    //constructor requires constants for extension and retraction of 3 solenoids that make up the arm 
     public ArmComponent()
     {
         this.trombone = new DoubleSolenoid(ElectronicsConstants.TROMBONE_SOLANOID_EXTEND, ElectronicsConstants.TROMBONE_SOLANOID_RETRACT);
@@ -20,18 +27,32 @@ public class ArmComponent
             ElectronicsConstants.EXTEND_LINK_SOLANOID_RETRACT);
     }
 
+    /**
+     * @param state to which to set the Trombone Solenoid
+     */
     public void setTromboneSolenoidState(Value state)
     {
         this.trombone.set(state);
+        SmartDashboardLogger.putNumber(ArmComponent.TROMBONE_STATE_LOG_KEY, trombone.get().value);
+
     }
 
+    /**
+     * @param state to which to set the MoveLinkage Solenoid
+     */
     public void setMoveLinkageSolenoidState(Value state)
     {
         this.moveLinkage.set(state);
+        SmartDashboardLogger.putNumber(ArmComponent.MOVE_LINKAGE_STATE_LOG_KEY, moveLinkage.get().value);
     }
 
+    /**
+     * @param state to which to set the ExtendLinkage Solenoid
+     */
     public void setExtendLinkageSolenoidState(Value state)
     {
         this.extendLinkage.set(state);
+        SmartDashboardLogger.putNumber(ArmComponent.EXTEND_LINKAGE_STATE_LOG_KEY, extendLinkage.get().value);
     }
+
 }
