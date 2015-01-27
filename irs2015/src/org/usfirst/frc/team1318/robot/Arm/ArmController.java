fb2af2edb22a001a9036ca6e73b652c0b5a7df61
Tali <Tali@Takas>
2015-01-26 19:07:22 -0800

    finished ArmController

--- irs2015/src/org/usfirst/frc/team1318/robot/Arm/ArmController.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/Arm/ArmController.java
@@ -1,8 +1,11 @@
 package org.usfirst.frc.team1318.robot.Arm;
 
+import org.usfirst.frc.team1318.robot.Common.IController;
 import org.usfirst.frc.team1318.robot.Common.IDriver;
 
-public class ArmController
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class ArmController implements IController
 {
     ArmComponent component;
     IDriver driver;
@@ -11,5 +14,59 @@ public class ArmController
     {
         this.component = component;
         this.driver = driver;
+    }
+
+    @Override
+    public void update()
+    {
+        //toggle state of ArmExtender solenoid if button pressed
+        if (this.driver.getArmExtenderToggleOverride())
+        {
+            switch (this.component.extendLinkage.get().value)
+            {
+                case Value.kForward_val:
+                    component.setExtendLinkageSolenoidState(false);
+                    break;
+                case Value.kReverse_val:
+                    component.setExtendLinkageSolenoidState(true);
+                    break;
+            }
+        }
+
+        //toggle state of ArmTilting solenoid if button pressed
+        if (this.driver.getArmTiltToggleOverride())
+        {
+            switch (this.component.moveLinkage.get().value)
+            {
+                case Value.kForward_val:
+                    component.setMoveLinkageSolenoidState(false);
+                    break;
+                case Value.kReverse_val:
+                    component.setMoveLinkageSolenoidState(true);
+                    break;
+            }
+        }
+
+        //toggle state of Trombone solenoid if button pressed
+        if (this.driver.getArmTromboneToggleOverride())
+        {
+            switch (this.component.trombone.get().value)
+            {
+                case Value.kForward_val:
+                    component.setTromboneSolenoidState(false);
+                    break;
+                case Value.kReverse_val:
+                    component.setTromboneSolenoidState(true);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void stop()
+    {
+        this.component.setExtendLinkageSolenoidState(false);
+        this.component.setMoveLinkageSolenoidState(false);
+        this.component.setTromboneSolenoidState(false);
     }
 }
