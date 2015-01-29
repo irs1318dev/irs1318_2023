8204effff518e956db31905e40561df87115483e
Tali <Tali@Takas>
2015-01-28 17:40:32 -0800

    finished arm

--- irs2015/src/org/usfirst/frc/team1318/robot/Arm/ArmController.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/Arm/ArmController.java
@@ -3,8 +3,6 @@ package org.usfirst.frc.team1318.robot.Arm;
 import org.usfirst.frc.team1318.robot.Common.IController;
 import org.usfirst.frc.team1318.robot.Common.IDriver;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-
 public class ArmController implements IController
 {
     ArmComponent component;
@@ -20,54 +18,58 @@ public class ArmController implements IController
     public void update()
     {
         this.component.setExtendLinkageSolenoidState(this.driver.getArmExtenderToggleOverride());
-        /*        toggle state of ArmExtender solenoid if button pressed
-                if (this.driver.getArmExtenderToggleOverride())
+        this.component.setTiltLinkageSolenoidState(this.driver.getArmTiltToggleOverride());
+        this.component.setTromboneSolenoidState(this.driver.getArmTromboneToggleOverride());
+
+        /*        this.component.setExtendLinkageSolenoidState(this.driver.getArmExtenderToggleOverride());
+                        toggle state of ArmExtender solenoid if button pressed
+                        if (this.driver.getArmExtenderToggleOverride())
+                        {
+                            switch (this.component.extendLinkage.get().value)
+                            {
+                                case Value.kForward_val:
+                                    component.setExtendLinkageSolenoidState(false);
+                                    break;
+                                case Value.kReverse_val:
+                                    component.setExtendLinkageSolenoidState(true);
+                                    break;
+                            }
+                        }
+
+                //toggle state of ArmTilting solenoid if button pressed
+                if (this.driver.getArmTiltToggleOverride())
                 {
-                    switch (this.component.extendLinkage.get().value)
+                    switch (this.component.moveLinkage.get().value)
                     {
                         case Value.kForward_val:
-                            component.setExtendLinkageSolenoidState(false);
+                            component.setMoveLinkageSolenoidState(false);
                             break;
                         case Value.kReverse_val:
-                            component.setExtendLinkageSolenoidState(true);
+                            component.setMoveLinkageSolenoidState(true);
                             break;
                     }
-                }*/
+                }
 
-        //toggle state of ArmTilting solenoid if button pressed
-        if (this.driver.getArmTiltToggleOverride())
-        {
-            switch (this.component.moveLinkage.get().value)
-            {
-                case Value.kForward_val:
-                    component.setMoveLinkageSolenoidState(false);
-                    break;
-                case Value.kReverse_val:
-                    component.setMoveLinkageSolenoidState(true);
-                    break;
-            }
-        }
-
-        //toggle state of Trombone solenoid if button pressed
-        if (this.driver.getArmTromboneToggleOverride())
-        {
-            switch (this.component.trombone.get().value)
-            {
-                case Value.kForward_val:
-                    component.setTromboneSolenoidState(false);
-                    break;
-                case Value.kReverse_val:
-                    component.setTromboneSolenoidState(true);
-                    break;
-            }
-        }
-    }
+                //toggle state of Trombone solenoid if button pressed
+                if (this.driver.getArmTromboneToggleOverride())
+                {
+                    switch (this.component.trombone.get().value)
+                    {
+                        case Value.kForward_val:
+                            component.setTromboneSolenoidState(false);
+                            break;
+                        case Value.kReverse_val:
+                            component.setTromboneSolenoidState(true);
+                            break;
+                    }
+                }
+        */}
 
     @Override
     public void stop()
     {
         this.component.setExtendLinkageSolenoidState(false);
-        this.component.setMoveLinkageSolenoidState(false);
+        this.component.setTiltLinkageSolenoidState(false);
         this.component.setTromboneSolenoidState(false);
     }
 }
