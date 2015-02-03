3229c8d186a49cd9e2d04a35aeb75bf522b5822e
Tali <Tali@Takas>
2015-02-02 21:49:48 -0800

    added comments and edited logic of wait times in case certain solenoids are already in place. This is a bit dangerous because we do not know how much time the solenoid has spent in the correct state - maybe it is just starting to open or close and it wouldn't be safe to proceed to the next step - I ensured the fast-forwarded time-comparison by adding a couple seconds of SAFETY time -> this requires approval from the mechanics

--- irs2015/src/org/usfirst/frc/team1318/robot/Arm/ArmController.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/Arm/ArmController.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc.team1318.robot.Arm;
 
-import java.util.Date;
-
 import org.usfirst.frc.team1318.robot.Common.IController;
 import org.usfirst.frc.team1318.robot.Common.IDriver;
+
+import edu.wpi.first.wpilibj.Timer;
 
 public class ArmController implements IController
 {
@@ -15,12 +15,13 @@ public class ArmController implements IController
     private ArmStates armstateExtendor;
     private ArmStates armstateRetractor;
 
-    private Date timer;
-    private long startTime;
-    private final long PARTIAL_EXTEND_WAIT_TIME = 5000;
-    private final long FULL_EXTEND_WAIT_TIME = this.PARTIAL_EXTEND_WAIT_TIME + 5000;
-    private final long PARTIAL_RETRACT_WAIT_TIME = 5000;
-    private final long FULL_RETRACT_WAIT_TIME = this.PARTIAL_RETRACT_WAIT_TIME + 5000;
+    private Timer timer;
+    private double startTime;
+    private final double PARTIAL_EXTEND_WAIT_TIME = 5;
+    private final double FULL_EXTEND_WAIT_TIME = 5;
+    private final double PARTIAL_RETRACT_WAIT_TIME = 5;
+    private final double FULL_RETRACT_WAIT_TIME = 5;
+    private final double SAFETY_WAIT = 2;
 
     private ArmComponent component;
     private IDriver driver;
@@ -36,50 +37,28 @@ public class ArmController implements IController
         this.component = component;
         this.driver = driver;
 
+        //in rest position: extendLinkage is extended, tiltLinkage is extended, trombone is retracted 
         this.tromboneState = false;
         this.tiltState = true;
         this.extenderState = true;
+
+        //initialize new Timer but start it between actual stages of arm-movement
+        this.timer = new Timer();
+        this.timer.start();
     }
 
     @Override
     public void update()
     {
-        //in rest position: extendLinkage is open, trombone is closed, tiltLinkage is open 
-        //Macro Retract state machine
-        switch (this.armstateRetractor)
-        {
-            case STAGE_0:
-                if (this.driver.getArmMacroRetractButton())
-                {
-                    this.armstateRetractor = ArmStates.STAGE_1;
-                    this.armstateExtendor = ArmStates.STAGE_0;
-                }
-                break;
-            //start the Retraction timer
-            case STAGE_1:
-                this.tiltState = true;
-                this.startTime = this.timer.getTime();
-                this.armstateRetractor = ArmStates.STAGE_2;
-                break;
-            case STAGE_2:
-                if (this.timer.getTime() - this.startTime >= this.PARTIAL_RETRACT_WAIT_TIME)
-                {
-                    this.tromboneState = false;
-                    this.armstateRetractor = ArmStates.STAGE_3;
-                }
-                break;
-            case STAGE_3:
-                if (this.timer.getTime() - this.startTime >= this.FULL_RETRACT_WAIT_TIME)
-                {
-                    this.extenderState = true;
-                    this.armstateRetractor = ArmStates.STAGE_0;
-                }
-                break;
-            default:
-                break;
-        }
+        /*WILL"S SUGGESTION FOR FUTURE CONSIDERATION: to check all of the buttons and determine the current user intent 
+         * (i.e. first check for extend macro, then retract macro, then each of the extend/retract overrides)
+         * before calculating any action you want to perform -  as opposed to currently convoluted logic
+         */
 
-        //Macro Extend state machine
+        /* 
+         * Macro Extend operation will retract the extender (Stage 1), extend the trombone (Stage 2), and then retract the tilt (Stage 3)
+         * Fully extends entire arm
+         */
         switch (this.armstateExtendor)
         {
             case STAGE_0:
@@ -89,21 +68,37 @@ public class ArmController implements IController
                     this.armstateRetractor = ArmStates.STAGE_0;
                 }
                 break;
-            //start the Extendor timer
             case STAGE_1:
-                this.extenderState = false;
-                this.startTime = this.timer.getTime();
+                if (this.extenderState == false)
+                {
+                    //don't wait for extender to retract
+                    this.startTime = this.timer.get() + this.PARTIAL_EXTEND_WAIT_TIME - this.SAFETY_WAIT;
+                }
+                else
+                {
+                    this.startTime = this.timer.get();
+                    this.extenderState = false;
+                }
                 this.armstateExtendor = ArmStates.STAGE_2;
                 break;
             case STAGE_2:
-                if (this.timer.getTime() - this.startTime >= this.PARTIAL_EXTEND_WAIT_TIME)
+                if (this.timer.get() - this.startTime >= this.PARTIAL_EXTEND_WAIT_TIME)
                 {
-                    this.tromboneState = true;
+                    if (this.tromboneState == true)
+                    {
+                        //don't wait for trombone to extend
+                        this.startTime = this.timer.get() + this.FULL_EXTEND_WAIT_TIME - this.SAFETY_WAIT;
+                    }
+                    else
+                    {
+                        this.startTime = this.timer.get();
+                        this.tromboneState = true;
+                    }
                     this.armstateExtendor = ArmStates.STAGE_3;
                 }
                 break;
             case STAGE_3:
-                if (this.timer.getTime() - this.startTime >= this.FULL_EXTEND_WAIT_TIME)
+                if (this.timer.get() - this.startTime >= this.FULL_EXTEND_WAIT_TIME)
                 {
                     this.tiltState = false;
                     this.armstateExtendor = ArmStates.STAGE_0;
@@ -113,6 +108,64 @@ public class ArmController implements IController
                 break;
         }
 
+        /* 
+         * Macro Retract operation extends the tilt (Stage 1), retracts the trombone (Stage 2), and extends the extender (Stage 3)
+         * Returns arm to most compact position
+         * The Retract operation takes precedence over the Extend operation.
+         */
+        switch (this.armstateRetractor)
+        {
+            case STAGE_0:
+                if (this.driver.getArmMacroRetractButton())
+                {
+                    this.armstateRetractor = ArmStates.STAGE_1;
+                    this.armstateExtendor = ArmStates.STAGE_0;
+                }
+                break;
+            case STAGE_1:
+                if (this.tiltState == true)
+                {
+                    //don't wait for tilt to extend
+                    this.startTime = this.timer.get() + this.PARTIAL_RETRACT_WAIT_TIME - this.SAFETY_WAIT;
+                }
+                else
+                {
+                    this.startTime = this.timer.get();
+                    this.tiltState = true;
+                }
+                this.armstateRetractor = ArmStates.STAGE_2;
+                break;
+            case STAGE_2:
+                if (this.timer.get() - this.startTime >= this.PARTIAL_RETRACT_WAIT_TIME)
+                {
+                    if (this.tromboneState == false)
+                    {
+                        //don't wait for trombone to retract
+                        this.startTime = this.timer.get() + this.FULL_RETRACT_WAIT_TIME - this.SAFETY_WAIT;
+                    }
+                    else
+                    {
+                        this.startTime = this.timer.get();
+                        this.tromboneState = false;
+                    }
+                    this.armstateRetractor = ArmStates.STAGE_3;
+                }
+                break;
+            case STAGE_3:
+                if (this.timer.get() - this.startTime >= this.FULL_RETRACT_WAIT_TIME)
+                {
+                    this.extenderState = true;
+                    this.armstateRetractor = ArmStates.STAGE_0;
+                }
+                break;
+            default:
+                break;
+        }
+
+        /* 
+         * Any override takes precedence over any macro
+         * The retract overrides take precedence over the corresponding extend overrides.
+         */
         if (this.driver.getArmExtenderExtendOverride())
         {
             this.extenderState = true;
@@ -152,16 +205,18 @@ public class ArmController implements IController
             this.armstateRetractor = ArmStates.STAGE_0;
         }
 
+        //actually set the solenoid states only once per update cycle in order to prevent opposite commands
         this.component.setExtendLinkageSolenoidState(extenderState);
         this.component.setTiltLinkageSolenoidState(tiltState);
         this.component.setTromboneSolenoidState(tromboneState);
     }
 
     @Override
+    //return to completely retracted position - NOT SURE??
     public void stop()
     {
-        this.component.setExtendLinkageSolenoidState(false);
-        this.component.setTiltLinkageSolenoidState(false);
+        this.component.setExtendLinkageSolenoidState(true);
+        this.component.setTiltLinkageSolenoidState(true);
         this.component.setTromboneSolenoidState(false);
     }
 }
