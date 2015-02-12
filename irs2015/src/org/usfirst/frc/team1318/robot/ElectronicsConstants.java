2bf28fa00dff3bd482ba658732f7ae025b0a0bad
WRall <wm_rall@hotmail.com>
2015-02-11 23:26:55 -0800

    Use practice robot's known electronics ports

--- irs2015/src/org/usfirst/frc/team1318/robot/ElectronicsConstants.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/ElectronicsConstants.java
@@ -8,22 +8,28 @@ package org.usfirst.frc.team1318.robot;
  */
 public class ElectronicsConstants
 {
-    public static final int DRIVETRAIN_LEFT_TALON_CHANNEL = 9;
-    public static final int DRIVETRAIN_RIGHT_TALON_CHANNEL = 8;
+    public static final int DRIVETRAIN_LEFT_TALON_CHANNEL = 3; // kitbot - 9
+    public static final int DRIVETRAIN_RIGHT_TALON_CHANNEL = 2; // kitbot - 8
 
-    public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_A = 6;
-    public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_B = 7;
+    public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_A = 4; // kitbot - 6
+    public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_B = 5; // kitbot - 7
 
-    public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_A = 8;
-    public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_B = 9;
+    public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_A = 2; // kitbot - 8
+    public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_B = 3; // kitbot - 9
 
-    public static final int COMPRESSOR_PRESSURE_SWITCH_CHANNEL = 6;
-    public static final int COMPRESSOR_RELAY_CHANNEL = 1;
+    public static final int ELEVATOR_TALON_CHANNEL = 4;
+    public static final int ELEVATOR_ENCODER_CHANNEL_A = 1;
+    public static final int ELEVATOR_ENCODER_CHANNEL_B = 0;
 
-    public static final int COMPRESSOR_ANALOG_PRESSURE_SENSOR = 4;
+    public static final int ELEVATOR_LOWER_LIMIT_SWITCH = 6;
+    public static final int ELEVATOR_UPPER_LIMIT_SWITCH = 7;
 
-    public static final int INTAKE_LEFT_TALON_CHANNEL = -1; // TODO
-    public static final int INTAKE_RIGHT_TALON_CHANNEL = -1; // TODO
+    public static final int ELEVATOR_THROUGH_BEAM_SENSOR_CHANNEL = -1;
+    public static final int ELEVATOR_HALL_EFFECT_TOP_CHANNEL = -1;
+    public static final int ELEVATOR_HALL_EFFECT_BOTTOM_CHANNEL = -1;
+
+    public static final int INTAKE_LEFT_TALON_CHANNEL = 1;
+    public static final int INTAKE_RIGHT_TALON_CHANNEL = 0;
 
     public static final int INTAKE_LEFT_ARM_CHANNEL_A = -1; // TODO
     public static final int INTAKE_LEFT_ARM_CHANNEL_B = -1; // TODO
@@ -31,6 +37,5 @@ public class ElectronicsConstants
     public static final int INTAKE_RIGHT_ARM_CHANNEL_A = -1; // TODO
     public static final int INTAKE_RIGHT_ARM_CHANNEL_B = -1; // TODO
 
-    public static final double COMPRESSOR_MAX_PSI = 150.0;
-    public static final double COMPRESSOR_MAX_VOLTAGE = 10.0;
+    public static final double MAX_POWER_LEVEL = 1.0;
 }
