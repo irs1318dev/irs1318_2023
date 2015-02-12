48ae5db912c0f6bfeab7b3a441c3477e6c629083
WRall <wm_rall@hotmail.com>
2015-02-11 23:24:28 -0800

    Update just the electronics constants that we know currently (no solenoids)

--- irs2015/src/org/usfirst/frc/team1318/robot/ElectronicsConstants.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/ElectronicsConstants.java
@@ -8,30 +8,35 @@ package org.usfirst.frc.team1318.robot;
  */
 public class ElectronicsConstants
 {
-    public static final int SOLENOID_MODULE_PORT_1 = 3;
-    public static final int SOLENOID_MODULE_PORT_2 = 2;
+    public static final int DRIVETRAIN_LEFT_TALON_CHANNEL = 3; // kitbot - 9
+    public static final int DRIVETRAIN_RIGHT_TALON_CHANNEL = 2; // kitbot - 8
 
-    public static final int DRIVETRAIN_LEFT_TALON_CHANNEL = 9;
-    public static final int DRIVETRAIN_RIGHT_TALON_CHANNEL = 8;
+    public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_A = 4; // kitbot - 6
+    public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_B = 5; // kitbot - 7
 
-    public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_A = 6;
-    public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_B = 7;
+    public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_A = 2; // kitbot - 8
+    public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_B = 3; // kitbot - 9
 
-    public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_A = 8;
-    public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_B = 9;
+    public static final int ELEVATOR_TALON_CHANNEL = 4;
+    public static final int ELEVATOR_ENCODER_CHANNEL_A = 1;
+    public static final int ELEVATOR_ENCODER_CHANNEL_B = 0;
 
-    public static final int COMPRESSOR_PRESSURE_SWITCH_CHANNEL = 6;
-    public static final int COMPRESSOR_RELAY_CHANNEL = 1;
+    public static final int ELEVATOR_LOWER_LIMIT_SWITCH = 6;
+    public static final int ELEVATOR_UPPER_LIMIT_SWITCH = 7;
 
-    public static final int COMPRESSOR_ANALOG_PRESSURE_SENSOR = 4;
+    public static final int ELEVATOR_THROUGH_BEAM_SENSOR_CHANNEL = -1;
+    public static final int ELEVATOR_HALL_EFFECT_TOP_CHANNEL = -1;
+    public static final int ELEVATOR_HALL_EFFECT_BOTTOM_CHANNEL = -1;
 
-    public static final double COMPRESSOR_MAX_PSI = 150.0;
-    public static final double COMPRESSOR_MAX_VOLTAGE = 10.0;
+    public static final int INTAKE_LEFT_TALON_CHANNEL = 1;
+    public static final int INTAKE_RIGHT_TALON_CHANNEL = 0;
 
-    public static final int TROMBONE_SOLANOID_EXTEND = 1;
-    public static final int TROMBONE_SOLANOID_RETRACT = 0;
-    public static final int TILT_LINK_SOLANOID_EXTEND = 5;
-    public static final int TILT_LINK_SOLANOID_RETRACT = 4;
-    public static final int EXTEND_LINK_SOLANOID_EXTEND = 3;
-    public static final int EXTEND_LINK_SOLANOID_RETRACT = 2;
+    public static final int TROMBONE_SOLANOID_EXTEND = 1; // TODO
+    public static final int TROMBONE_SOLANOID_RETRACT = 0; // TODO
+    public static final int TILT_LINK_SOLANOID_EXTEND = 5; // TODO
+    public static final int TILT_LINK_SOLANOID_RETRACT = 4; // TODO
+    public static final int EXTEND_LINK_SOLANOID_EXTEND = 3; // TODO
+    public static final int EXTEND_LINK_SOLANOID_RETRACT = 2; // TODO
+
+    public static final double MAX_POWER_LEVEL = 1.0;
 }
