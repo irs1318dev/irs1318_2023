769cb819164bc86417e1126f1ba0a01a0776751c
IRS1318 <IRS1318@DriveStation>
2015-02-14 13:55:51 -0800

    Add updateState for MoveToBottom functionality (for co-driver)

--- irs2015/src/org/usfirst/frc/team1318/robot/UserInterface/UserDriver.java
+++ irs2015/src/org/usfirst/frc/team1318/robot/UserInterface/UserDriver.java
@@ -204,6 +204,7 @@ public class UserDriver implements IDriver
         this.elevatorIgnoreSensors.updateState(this.joystickCoDriver.getRawButton(JoystickButtonConstants.ELEVATOR_IGNORE_SENSORS_BUTTON));
         this.elevatorUseSensors.updateState(this.joystickCoDriver.getRawButton(JoystickButtonConstants.ELEVATOR_USE_SENSORS_BUTTON));
         this.elevatorZeroEncoders.updateState(this.joystickCoDriver.getRawButton(JoystickButtonConstants.ELEVATOR_ZERO_ENCODERS));
+        this.elevatorMoveToBottom.updateState(this.joystickCoDriver.getRawButton(JoystickButtonConstants.ELEVATOR_MOVE_TO_BOTTOM));
     }
 
     /**
