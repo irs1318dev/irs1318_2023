ef6e00d60d9ed0e96f404fc2c322c76d47547aff
DriverStationA <DriverStationA>
2017-11-30 20:19:20 -0800

    UnitTest fixes part 1

--- irs1318_general/test/org/usfirst/frc/team1318/robot/drivetrain/DriveTrainMechanismTest.java
+++ irs1318_general/test/org/usfirst/frc/team1318/robot/drivetrain/DriveTrainMechanismTest.java
@@ -8,12 +8,11 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import org.junit.Test;
 import org.usfirst.frc.team1318.robot.ElectronicsConstants;
-import org.usfirst.frc.team1318.robot.HardwareConstants;
 import org.usfirst.frc.team1318.robot.TestWpilibProvider;
+import org.usfirst.frc.team1318.robot.TuningConstants;
 import org.usfirst.frc.team1318.robot.common.IDashboardLogger;
-import org.usfirst.frc.team1318.robot.common.wpilib.IEncoder;
-import org.usfirst.frc.team1318.robot.common.wpilib.IMotor;
-import org.usfirst.frc.team1318.robot.common.wpilib.ITimer;
+import org.usfirst.frc.team1318.robot.common.wpilib.CANTalonControlMode;
+import org.usfirst.frc.team1318.robot.common.wpilib.ICANTalon;
 import org.usfirst.frc.team1318.robot.driver.Operation;
 import org.usfirst.frc.team1318.robot.driver.common.Driver;
 
@@ -23,20 +22,15 @@ public class DriveTrainMechanismTest
     public void testSetPower_Zero()
     {
         IDashboardLogger logger = mock(IDashboardLogger.class);
-        ITimer timer = mock(ITimer.class);
         TestWpilibProvider testProvider = new TestWpilibProvider();
-        IMotor leftMotor = testProvider.getTalon(ElectronicsConstants.DRIVETRAIN_LEFT_TALON_CHANNEL);
-        IMotor rightMotor = testProvider.getTalon(ElectronicsConstants.DRIVETRAIN_RIGHT_TALON_CHANNEL);
-        IEncoder leftEncoder = testProvider.getEncoder(
-            ElectronicsConstants.DRIVETRAIN_LEFT_ENCODER_CHANNEL_A,
-            ElectronicsConstants.DRIVETRAIN_LEFT_ENCODER_CHANNEL_B);
-        IEncoder rightEncoder = testProvider.getEncoder(
-            ElectronicsConstants.DRIVETRAIN_RIGHT_ENCODER_CHANNEL_A,
-            ElectronicsConstants.DRIVETRAIN_RIGHT_ENCODER_CHANNEL_B);
+        ICANTalon leftMotor = testProvider.getCANTalon(ElectronicsConstants.DRIVETRAIN_LEFT_MOTOR_CHANNEL);
+        ICANTalon rightMotor = testProvider.getCANTalon(ElectronicsConstants.DRIVETRAIN_RIGHT_MOTOR_CHANNEL);
+        ICANTalon leftFollowerMotor = testProvider.getCANTalon(ElectronicsConstants.DRIVETRAIN_LEFT_FOLLOWER_CHANNEL);
+        ICANTalon rightFollowerMotor = testProvider.getCANTalon(ElectronicsConstants.DRIVETRAIN_RIGHT_FOLLOWER_CHANNEL);
 
         Driver driver = mock(Driver.class);
 
-        DriveTrainMechanism driveTrainMechanism = new DriveTrainMechanism(logger, timer, testProvider);
+        DriveTrainMechanism driveTrainMechanism = new DriveTrainMechanism(logger, testProvider);
         driveTrainMechanism.setDriver(driver);
 
         doReturn(false).when(driver).getDigital(Operation.DriveTrainDisablePID);
@@ -48,62 +42,91 @@ public class DriveTrainMechanismTest
         doReturn(false).when(driver).getDigital(Operation.DriveTrainSimpleMode);
         doReturn(0.0).when(driver).getAnalog(Operation.DriveTrainMoveForward);
         doReturn(0.0).when(driver).getAnalog(Operation.DriveTrainTurn);
-        doReturn(0.0).when(rightEncoder).getDistance();
-        doReturn(0.0).when(rightEncoder).getRate();
-        doReturn(0).when(rightEncoder).get();
-        doReturn(0.0).when(leftEncoder).getDistance();
-        doReturn(0.0).when(leftEncoder).getRate();
-        doReturn(0).when(leftEncoder).get();
-        doReturn(0.0).when(timer).get();
+        doReturn(0.0).when(leftMotor).getError();
+        doReturn(0.0).when(leftMotor).getSpeed();
+        doReturn(0).when(leftMotor).getTicks();
+        doReturn(0.0).when(rightMotor).getError();
+        doReturn(0.0).when(rightMotor).getSpeed();
+        doReturn(0).when(rightMotor).getTicks();
 
         driveTrainMechanism.update();
 
         verify(leftMotor).set(eq(0.0));
-        verify(rightMotor).set(eq(-0.0));
-        verify(leftEncoder).getDistance();
-        verify(leftEncoder).getRate();
-        verify(leftEncoder).get();
-        verify(rightEncoder).getDistance();
-        verify(rightEncoder).getRate();
-        verify(rightEncoder).get();
-        verify(leftEncoder).setDistancePerPulse(eq(HardwareConstants.DRIVETRAIN_LEFT_PULSE_DISTANCE));
-        verify(rightEncoder).setDistancePerPulse(eq(HardwareConstants.DRIVETRAIN_RIGHT_PULSE_DISTANCE));
+        verify(rightMotor).set(eq(0.0));
+        verify(leftMotor).getError();
+        verify(leftMotor).getSpeed();
+        verify(leftMotor).getTicks();
+        verify(rightMotor).getError();
+        verify(rightMotor).getSpeed();
+        verify(rightMotor).getTicks();
+
+        verify(leftMotor).enableBrakeMode(eq(false));
+        verify(leftMotor).reverseOutput(eq(false));
+        verify(leftMotor).reverseSensor(eq(true));
+        verify(leftFollowerMotor).enableBrakeMode(eq(false));
+        verify(leftFollowerMotor).reverseOutput(eq(false));
+        verify(leftFollowerMotor).changeControlMode(eq(CANTalonControlMode.Follower));
+        verify(leftFollowerMotor).set(eq((double)ElectronicsConstants.DRIVETRAIN_LEFT_MOTOR_CHANNEL));
+        verify(rightMotor).enableBrakeMode(eq(false));
+        verify(rightMotor).reverseOutput(eq(true));
+        verify(rightMotor).reverseSensor(eq(false));
+        verify(rightFollowerMotor).changeControlMode(eq(CANTalonControlMode.Follower));
+        verify(rightFollowerMotor).enableBrakeMode(eq(false));
+        verify(rightFollowerMotor).reverseOutput(eq(true));
+        verify(rightFollowerMotor).set(eq((double)ElectronicsConstants.DRIVETRAIN_RIGHT_MOTOR_CHANNEL));
+        verify(leftMotor).setPIDF(
+            eq(TuningConstants.DRIVETRAIN_VELOCITY_PID_LEFT_KP),
+            eq(TuningConstants.DRIVETRAIN_VELOCITY_PID_LEFT_KI),
+            eq(TuningConstants.DRIVETRAIN_VELOCITY_PID_LEFT_KD),
+            eq(TuningConstants.DRIVETRAIN_VELOCITY_PID_LEFT_KF));
+        verify(rightMotor).setPIDF(
+            eq(TuningConstants.DRIVETRAIN_VELOCITY_PID_RIGHT_KP),
+            eq(TuningConstants.DRIVETRAIN_VELOCITY_PID_RIGHT_KI),
+            eq(TuningConstants.DRIVETRAIN_VELOCITY_PID_RIGHT_KD),
+            eq(TuningConstants.DRIVETRAIN_VELOCITY_PID_RIGHT_KF));
         verifyNoMoreInteractions(leftMotor);
         verifyNoMoreInteractions(rightMotor);
-        verifyNoMoreInteractions(leftEncoder);
-        verifyNoMoreInteractions(rightEncoder);
+        verifyNoMoreInteractions(leftFollowerMotor);
+        verifyNoMoreInteractions(rightFollowerMotor);
     }
 
     @Test
     public void testStop()
     {
         IDashboardLogger logger = mock(IDashboardLogger.class);
-        ITimer timer = mock(ITimer.class);
         TestWpilibProvider testProvider = new TestWpilibProvider();
-        IMotor leftMotor = testProvider.getTalon(ElectronicsConstants.DRIVETRAIN_LEFT_TALON_CHANNEL);
-        IMotor rightMotor = testProvider.getTalon(ElectronicsConstants.DRIVETRAIN_RIGHT_TALON_CHANNEL);
-        IEncoder leftEncoder = testProvider.getEncoder(
-            ElectronicsConstants.DRIVETRAIN_LEFT_ENCODER_CHANNEL_A,
-            ElectronicsConstants.DRIVETRAIN_LEFT_ENCODER_CHANNEL_B);
-        IEncoder rightEncoder = testProvider.getEncoder(
-            ElectronicsConstants.DRIVETRAIN_RIGHT_ENCODER_CHANNEL_A,
-            ElectronicsConstants.DRIVETRAIN_RIGHT_ENCODER_CHANNEL_B);
+        ICANTalon leftMotor = testProvider.getCANTalon(ElectronicsConstants.DRIVETRAIN_LEFT_MOTOR_CHANNEL);
+        ICANTalon rightMotor = testProvider.getCANTalon(ElectronicsConstants.DRIVETRAIN_RIGHT_MOTOR_CHANNEL);
+        ICANTalon leftFollowerMotor = testProvider.getCANTalon(ElectronicsConstants.DRIVETRAIN_LEFT_FOLLOWER_CHANNEL);
+        ICANTalon rightFollowerMotor = testProvider.getCANTalon(ElectronicsConstants.DRIVETRAIN_RIGHT_FOLLOWER_CHANNEL);
 
         Driver driver = mock(Driver.class);
 
-        DriveTrainMechanism driveTrainMechanism = new DriveTrainMechanism(logger, timer, testProvider);
+        DriveTrainMechanism driveTrainMechanism = new DriveTrainMechanism(logger, testProvider);
         driveTrainMechanism.setDriver(driver);
         driveTrainMechanism.stop();
 
         verify(leftMotor).set(eq(0.0));
         verify(rightMotor).set(eq(0.0));
-        verify(leftEncoder).reset();
-        verify(rightEncoder).reset();
-        verify(leftEncoder).setDistancePerPulse(eq(HardwareConstants.DRIVETRAIN_LEFT_PULSE_DISTANCE));
-        verify(rightEncoder).setDistancePerPulse(eq(HardwareConstants.DRIVETRAIN_RIGHT_PULSE_DISTANCE));
+        verify(rightMotor).reset();
+        verify(leftMotor).reset();
+        verify(leftMotor).enableBrakeMode(eq(false));
+        verify(leftMotor).reverseOutput(eq(false));
+        verify(leftMotor).reverseSensor(eq(true));
+        verify(leftFollowerMotor).enableBrakeMode(eq(false));
+        verify(leftFollowerMotor).reverseOutput(eq(false));
+        verify(leftFollowerMotor).changeControlMode(eq(CANTalonControlMode.Follower));
+        verify(leftFollowerMotor).set(eq(ElectronicsConstants.DRIVETRAIN_LEFT_MOTOR_CHANNEL));
+        verify(rightMotor).enableBrakeMode(eq(false));
+        verify(rightMotor).reverseOutput(eq(true));
+        verify(rightMotor).reverseSensor(eq(false));
+        verify(rightFollowerMotor).changeControlMode(eq(CANTalonControlMode.Follower));
+        verify(rightFollowerMotor).enableBrakeMode(eq(false));
+        verify(rightFollowerMotor).reverseOutput(eq(true));
+        verify(rightFollowerMotor).set(eq(ElectronicsConstants.DRIVETRAIN_RIGHT_MOTOR_CHANNEL));
         verifyNoMoreInteractions(leftMotor);
         verifyNoMoreInteractions(rightMotor);
-        verifyNoMoreInteractions(leftEncoder);
-        verifyNoMoreInteractions(rightEncoder);
+        verifyNoMoreInteractions(leftFollowerMotor);
+        verifyNoMoreInteractions(rightFollowerMotor);
     }
 }
