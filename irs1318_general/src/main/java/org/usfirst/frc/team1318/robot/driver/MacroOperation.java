0adef70c0e6a63bef167cf4e0cb043b669519b4d
William Rall <William Rall>
2018-07-01 14:51:25 -0700

    Use GradleRIO
    Switch to GradleRIO in preparation for 2019

---
+++ irs1318_general/src/main/java/org/usfirst/frc/team1318/robot/driver/MacroOperation.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team1318.robot.driver;
+
+public enum MacroOperation
+{
+    // DriveTrain operations:
+    PIDBrake,
+    VisionCenter,
+    VisionCenterAndAdvance,
+    DriveForwardTurnRight,
+}
---
+++ irs1318_general/src/main/java/org/usfirst/frc/team1318/robot/driver/Shift.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team1318.robot.driver;
+
+public enum Shift
+{
+    None,
+    Any,
+    Debug,
+}
