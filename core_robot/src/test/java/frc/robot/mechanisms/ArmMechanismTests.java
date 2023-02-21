9badbb6762e2ff5d1543a06a794c82ab039c866e
William Rall <William Rall>
2023-02-20 20:40:32 -0800

    Finalize more pieces for the arm forward/inverse kinematics

--- core_robot/src/test/java/frc/robot/mechanisms/ArmMechanismTests.java
+++ core_robot/src/test/java/frc/robot/mechanisms/ArmMechanismTests.java
@@ -1,7 +1,6 @@
 package frc.robot.mechanisms;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -16,42 +15,10 @@ import frc.robot.common.Helpers;
 public class ArmMechanismTests
 {
     @Test
-    public void calculateAngleStraightUpAndDown()
-    {
-        DoubleTuple setpoint = ArmMechanism.calculateIKAnglesFromPosition(0.0, Math.abs(HardwareConstants.ARM_LOWER_ARM_LENGTH - HardwareConstants.ARM_UPPER_ARM_LENGTH));
-        assertEquals(90.0, setpoint.first, 0.01, "lower angle");
-        assertEquals(0.0, setpoint.second, 0.01, "upper angle");
-    }
-
-    @Test
-    public void verifycalculateAngleStraightUpAndDownFK()
-    {
-        DoubleTuple setpoint = ArmMechanism.calculateFKPositionsFromAngles(90.0, 0.0);
-        // assertEquals(0.0, Math.abs(HardwareConstants.ARM_LOWER_ARM_LENGTH - setpoint.first));
-        // assertEquals(Math.abs(HardwareConstants.ARM_LOWER_ARM_LENGTH - HardwareConstants.ARM_UPPER_ARM_LENGTH), setpoint.second);
-    }
-
-    @Test
-    public void calculateAngleStraightOut()
-    {
-        DoubleTuple setpoint = ArmMechanism.calculateIKAnglesFromPosition(HardwareConstants.ARM_UPPER_ARM_LENGTH + HardwareConstants.ARM_LOWER_ARM_LENGTH, 0.0);
-        assertEquals(0.0, setpoint.first, 0.01, "lower angle");
-        assertEquals(180.0, setpoint.second, 0.01, "upper angle");
-    }
-
-    @Test
     public void calculateAngleStraightUpAndOver()
     {
         DoubleTuple setpoint = ArmMechanism.calculateIKAnglesFromPosition(HardwareConstants.ARM_UPPER_ARM_LENGTH, HardwareConstants.ARM_LOWER_ARM_LENGTH);
         assertEquals(90.0, setpoint.first, 0.01, "lower angle");
-        assertEquals(90.0, setpoint.second, 0.01, "upper angle");
-    }
-
-    @Test
-    public void calculateAngleStraightOverAndDown()
-    {
-        DoubleTuple setpoint = ArmMechanism.calculateIKAnglesFromPosition(HardwareConstants.ARM_LOWER_ARM_LENGTH, -HardwareConstants.ARM_UPPER_ARM_LENGTH);
-        assertEquals(0.0, setpoint.first, 0.01, "lower angle");
         assertEquals(90.0, setpoint.second, 0.01, "upper angle");
     }
 
@@ -99,87 +66,53 @@ public class ArmMechanismTests
     @Test
     public void verifyFKLengthsEqualsIKLengths()
     {
-        final double maxExtension = (HardwareConstants.ARM_EXTENTION_LENGTH - 0.25) * HardwareConstants.ARM_STRING_ENCODER_TICKS_PER_INCH;
-        for (double lowerExtension = 0.5 * HardwareConstants.ARM_STRING_ENCODER_TICKS_PER_INCH; lowerExtension <= maxExtension; lowerExtension += 10.0)
+        final double maxExtension = (HardwareConstants.ARM_EXTENTION_LENGTH) * HardwareConstants.ARM_STRING_ENCODER_TICKS_PER_INCH;
+        for (double lowerExtension = 0.0 * HardwareConstants.ARM_STRING_ENCODER_TICKS_PER_INCH; lowerExtension <= maxExtension; lowerExtension += 10.0)
         {
-            for (double upperExtension = 0.5 * HardwareConstants.ARM_STRING_ENCODER_TICKS_PER_INCH; upperExtension <= maxExtension; upperExtension += 10.0)
+            for (double upperExtension = 0.0 * HardwareConstants.ARM_STRING_ENCODER_TICKS_PER_INCH; upperExtension <= maxExtension; upperExtension += 10.0)
             {
                 DoubleTuple angles = ArmMechanism.calculateFKAnglesFromExtensions(lowerExtension, upperExtension);
                 if (angles != null)
                 {
                     DoubleTuple extensions = ArmMechanism.calculateIKExtensionsFromAngles(angles.first, angles.second);
-                    assertNotNull(extensions);
-                    assertEquals(lowerExtension, extensions.first, 0.01, "lowerExtension");
-                    assertEquals(upperExtension, extensions.second, 0.01, "upperExtension");
+                    if (extensions != null)
+                    {
+                        assertEquals(lowerExtension, extensions.first, 0.01, "lowerExtension");
+                        assertEquals(upperExtension, extensions.second, 0.01, "upperExtension");
+                    }
                 }
             }
         }
     }
 
-
-    @Test
-    public void verifyLinearActuatorLengthsStraightUp()
-    {
-        DoubleTuple setpoint = ArmMechanism.calculateIKExtensionsFromAngles(90.0, 135.0);
-        // assertEquals(8.0 * HardwareConstants.ARM_STRING_ENCODER_TICKS_PER_INCH, setpoint.second, "Upper LA Length");
-        // assertEquals(8.0 * HardwareConstants.ARM_STRING_ENCODER_TICKS_PER_INCH, setpoint.first, "Lower LA Length");
-    }
-
-    @Test
-    public void FullUnitTestForStraightOut()
-    {
-        DoubleTuple IK = ArmMechanism.calculateIK(HardwareConstants.ARM_LOWER_ARM_LENGTH, HardwareConstants.ARM_UPPER_ARM_LENGTH);
-        DoubleTuple FK = ArmMechanism.calculateFK(IK.first, IK.second);
-        // assertEquals(HardwareConstants.ARM_LOWER_ARM_LENGTH, FK.first);
-        // assertEquals(HardwareConstants.ARM_UPPER_ARM_LENGTH, FK.second);
-        
-
-    }
-
-    @Test
-    public void verifyLAFKForFullyRetracted() //StraightOverAnd Down
-    {
-        DoubleTuple LA_to_Angles_out = ArmMechanism.calculateFKAnglesFromExtensions(16.93, 16.93);
-        // assertEquals(40, LA_to_Angles_out.first, "Lower Arm Angle Out");
-        // assertEquals(20, LA_to_Angles_out.second, "Upper Arm Angle Out");
-    }
-
     public static void main(String[] args)
     {
         // Generate CSV file containing valid positions for the arm given the IK/FK (and that they agree)
-        try (CsvWriter validCsvWriter = CsvWriter.builder().build(java.nio.file.Path.of("validPositions.csv"), StandardCharsets.UTF_8))
+        try (CsvWriter validCsvWriter = CsvWriter.builder().build(java.nio.file.Path.of("validPositions-reachConstrained.csv"), StandardCharsets.UTF_8))
         {
-            try (CsvWriter problematicCsvWriter = CsvWriter.builder().build(java.nio.file.Path.of("problematicPositions.csv"), StandardCharsets.UTF_8))
-            {
-                validCsvWriter.writeRow("x", "y", "lowerExtension", "upperExtension");
-                problematicCsvWriter.writeRow("x", "y", "lowerExtension", "upperExtension", "x_FK", "y_FK");
+            validCsvWriter.writeRow("x", "z", "lowerExtension", "upperExtension");
 
-                final double theoreticalFullExtension = HardwareConstants.ARM_LOWER_ARM_LENGTH + HardwareConstants.ARM_UPPER_ARM_LENGTH;
-                for (double x = -theoreticalFullExtension; x <= theoreticalFullExtension; x += 0.1)
+            final double theoreticalFullExtension = HardwareConstants.ARM_LOWER_ARM_LENGTH + HardwareConstants.ARM_UPPER_ARM_LENGTH;
+            for (double x = -theoreticalFullExtension; x <= theoreticalFullExtension; x += 0.1)
+            {
+                for (double z = -theoreticalFullExtension; z <= theoreticalFullExtension; z += 0.1)
                 {
-                    for (double z = -theoreticalFullExtension; z <= theoreticalFullExtension; z += 0.1)
+                    DoubleTuple setpoint = ArmMechanism.calculateIK(x, z);
+                    if (setpoint != null)
                     {
-                        DoubleTuple setpoint = ArmMechanism.calculateIK(x, z);
-                        if (setpoint != null)
+                        DoubleTuple result = ArmMechanism.calculateFK(setpoint.first, setpoint.second);
+                        if (result != null &&
+                            Helpers.RoughEquals(x, result.first, 0.01) &&
+                            Helpers.RoughEquals(z, result.second, 0.01))
                         {
-                            DoubleTuple result = ArmMechanism.calculateFK(setpoint.first, setpoint.second);
-                            if (result != null &&
-                                Helpers.RoughEquals(x, result.first, 0.01) &&
-                                Helpers.RoughEquals(z, result.second, 0.01))
-                            {
-                                validCsvWriter.writeRow(String.valueOf(x), String.valueOf(z), String.valueOf(setpoint.first), String.valueOf(setpoint.second));
-                            }
-                            else
-                            {
-                                problematicCsvWriter.writeRow(String.valueOf(x), String.valueOf(z), String.valueOf(setpoint.first), String.valueOf(setpoint.second), result == null ? "" : String.valueOf(result.first), result == null ? "" : String.valueOf(result.second));
-                            }
+                            validCsvWriter.writeRow(String.valueOf(x), String.valueOf(z), String.valueOf(setpoint.first), String.valueOf(setpoint.second));
+                        }
+                        else
+                        {
+                            throw new RuntimeException("Unsupported position " + x + ", " + z);
                         }
                     }
                 }
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace(System.err);
             }
         }
         catch (IOException e)
@@ -203,8 +136,6 @@ public class ArmMechanismTests
         //             }
         //         }
         //     }
-
-        //     csvWriter.close();
         // }
         // catch (IOException e)
         // {
